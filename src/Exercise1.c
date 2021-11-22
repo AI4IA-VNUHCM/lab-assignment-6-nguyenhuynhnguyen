5525a1f6c86d7fff077a7285b0b92a7b1b6247f6
nguyenhuynhnguyen <91869289+nguyenhuynhnguyen@users.noreply.github.com>
2021-11-22 15:31:12 +0700

    Update ex1 an d2

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -16,7 +16,7 @@ char *parseBasicNumber(int number) {
   return nums[number - 1];
 }
 char *parseEnhancedNumber(int number) {
-  char *nums[] = {"_",   "twen",  "third", "four", "five",
+  char *nums[] = {"_",   "twen",  "third", "four", "fifth",
                   "six", "seven", "eight", "nine"};
   return nums[number - 1];
 }
--- src/Exercise2.c
+++ src/Exercise2.c
@@ -12,57 +12,22 @@ Ex:
 #include <stdlib.h>
 #include <string.h>
 
-int parseBasicNumber(char *str) {
-  if (strcmp(str, "zero") == 0) {
-    return 0;
-  } else if (strcmp(str, "one") == 0) {
-    return 1;
-  } else if (strcmp(str, "two") == 0) {
-    return 2;
-  } else if (strcmp(str, "three") == 0) {
-    return 3;
-  } else if (strcmp(str, "four") == 0) {
-    return 4;
-  } else if (strcmp(str, "five") == 0) {
-    return 5;
-  } else if (strcmp(str, "six") == 0) {
-    return 6;
-  } else if (strcmp(str, "seven") == 0) {
-    return 7;
-  } else if (strcmp(str, "eight") == 0) {
-    return 8;
-  } else if (strcmp(str, "nine") == 0) {
-    return 9;
-  } else {
-    return -1;
-  }
-  return -1;
-}
-
-char *parseBasicNumberWords(int number) {
-  char *nums[] = {"one",   "two",   "three", "four", "five",   "six",
-                  "seven", "eight", "nine",  "ten",  "eleven", "twelve"};
-  return nums[number - 1];
-}
-
 void Ex2(int n, char *str[]) {
-  // Your codes here
-  int orderArray[n];
-  for (int i = 0; i < n; i++) {
-    orderArray[i] = parseBasicNumber(str[i]);
-  }
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n - 1; j++) {
-      if (orderArray[j] > orderArray[j + 1]) {
-        int temp = orderArray[j];
-        orderArray[j] = orderArray[j + 1];
-        orderArray[j + 1] = temp;
+  // Compare two string in element
+  for (int i = 0; i < n; i++)
+    for (int j = i + 1; j < n; j++)
+      if (strcmp(str[i], str[j]) > 0) {
+        char *tmp = str[i];
+        str[i] = str[j];
+        str[j] = tmp;
       }
-    }
-  }
-  for (int i = 0; i < n; i++) {
-    printf("%s ", parseBasicNumberWords(orderArray[i]));
-  }
+
+  // Print result
+  for (int i = 0; i < n; i++)
+    if (i < n - 1)
+      printf("%s ", str[i]);
+    else
+      printf("%s", str[i]);
 }
 
 int main(int argc, char *argv[]) {
